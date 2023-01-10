283d03befab8665176a5fe8303203252a4a5090e
Chanakya192110649 <112469749+Chanakya192110649@users.noreply.github.com>
2023-01-10 09:49:27 +0530

    Add files via upload

---
+++ ex.3.py
@@ -0,0 +1,7 @@
+print("\nAddition Result:")
+    for i in range(rowOne):
+        for j in range(colOne):
+            print(matThree[i][j], end=" ")
+        print()
+else:
+    print("\nDimension Mismatched!")
