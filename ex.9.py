77140188ae514ff5f8d4a9a751d786368662814d
Chanakya192110649 <112469749+Chanakya192110649@users.noreply.github.com>
2023-01-12 08:07:24 +0530

    Create ex.9.py

---
+++ ex.9.py
@@ -0,0 +1,12 @@
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+my_str = "Hello!!!, he said ---and went."
+
+
+no_punct = ""
+for char in my_str:
+   if char not in punctuations:
+       no_punct = no_punct + char
+print(my_str)
+print("removed",no_punct)
