4c78c05f2df2d3d49e020a10fa3ba5777c2ec148
Chanakya192110649 <112469749+Chanakya192110649@users.noreply.github.com>
2023-01-12 08:02:32 +0530

    Create ex.7.py

---
+++ ex.7.py
@@ -0,0 +1,8 @@
+A = {1, 2, 3, 5, 7, 9}
+B = {2, 4, 6, 7, 9, 0}
+
+
+print('Union of A and B:', A | B)
+print('Intersection of A and B:', A & B)
+print('Difference of A and B:', A - B)
+print('Symmetric Difference of A and B:', A ^ B)
